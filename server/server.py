ac231d3004b097bc26a374878d91df12ce171051
Mohammad Fahmi <fahmi17.mf9@gmail.com>
2025-02-05 08:20:14 +0700

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import json
 import random
-from learning import QLearning
+from server.learning import QLearning
 
 app = Flask(__name__)
 q_agent = QLearning()
