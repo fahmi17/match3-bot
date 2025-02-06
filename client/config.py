7a0beac521f5e75a3c83feee606e742446145bfa
Mohammad Fahmi <fahmi17.mf9@gmail.com>
2025-02-07 00:10:57 +0700

    Create config.py

---
+++ client/config.py
@@ -0,0 +1,3 @@
+# config.py (client)
+GAME_REGION = (100, 200, 800, 900)  # (x1, y1, x2, y2) Sesuaikan dengan posisi game di emulator
+SERVER_URL = "https://your-render-url.onrender.com"  # Ganti dengan URL server setelah deploy
